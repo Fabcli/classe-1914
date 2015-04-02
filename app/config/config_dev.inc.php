48fe75e9cfaa4077669f5abd5ce78cd3c6bb3c8a
GregDesplaces <contact@gregdesplaces.com>
2015-04-02 17:42:43 +0200

    Update the 'media_url' setting from "/" to "/medias/"

--- app/config/config_dev.inc.php
+++ app/config/config_dev.inc.php
@@ -14,6 +14,7 @@ $app->configureMode('development', function () use ($app) {
         'cache'                => false,
         // assets
         'static_url'           => "/",
+        'media_url'           => "/medias/",
         // after this date, switch to the game home page
         'launching_date'       => "2015-01-01T10:00:00"
     ));
