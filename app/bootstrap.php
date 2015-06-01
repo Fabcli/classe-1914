e41879d1bca67dae8fe93d28aafbb2674188bbd1
GregDesplaces <contact@gregdesplaces.com>
2015-06-01 15:09:56 +0200

    add demo config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/autoload.php';
 
 //Load environment config
 require_once __DIR__ . '/config/config_dev.inc.php';
+require_once __DIR__ . '/config/config_demo.inc.php';
 require_once __DIR__ . '/config/config_prod.inc.php';
 
 require_once __DIR__ . '/filters.php';
